199787355d2cfee2bb35775b35003be54e5cf633
tungdq212 <tung.duongquang@pixta.co.jp>
2024-08-09 09:17:35 +0000

    Update custom prompts

--- train.sh
+++ train.sh
@@ -1,6 +1,7 @@
-CUDA_VISIBLE_DEVICES="0" \
+CUDA_VISIBLE_DEVICES="1" \
 python trainscripts/textsliders/train_lora.py \
-    --name 'age' \
+    --name 'veronika_age_guidance_scale_1' \
     --rank 4 --alpha 1 \
     --config_file 'trainscripts/textsliders/data/custom/config.yaml' \
-    --prompts_file 'trainscripts/textsliders/data/custom/age_posneg.yaml'
+    --prompts_file 'trainscripts/textsliders/data/custom/veronika_age.yaml' \
+    # --attributes "full body, upper body, focus on face; front view, side view, back view" \
